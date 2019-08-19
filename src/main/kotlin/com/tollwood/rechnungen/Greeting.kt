b85cc28f62c77dd39b942bc8d54a8480d656f018
Tobias Schlüter <tobias.schlueter@sabio.de>
2019-08-19 09:11:12 +0200

    first commit

---
+++ src/main/kotlin/com/tollwood/rechnungen/Greeting.kt
@@ -0,0 +1,3 @@
+package com.tollwood.rechnungen
+
+data class Greeting(val id: Long, val content: String)
---
+++ src/main/kotlin/com/tollwood/rechnungen/RechnungenApplication.kt
@@ -0,0 +1,11 @@
+package com.tollwood.rechnungen
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RechnungenApplication
+
+fun main(args: Array<String>) {
+	runApplication<RechnungenApplication>(*args)
+}
