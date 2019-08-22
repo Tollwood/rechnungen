332260d9d87d54f54707fd969631aa6b779ef16a
Tobias Schlüter <tobias.schlueter@sabio.de>
2019-08-22 07:06:26 +0200

    - remove unused code

--- src/main/kotlin/com/tollwood/rechnungen/Address.kt
+++
@@ -1,8 +0,0 @@
-package com.tollwood.rechnungen
-
-import javax.persistence.Embeddable
-
-@Embeddable
-data class Address(val street: String, val housenumber: String, val zipCode: String, val city: String){
-
-}
--- src/main/kotlin/com/tollwood/rechnungen/EmployeeResource.kt
+++
@@ -1,8 +0,0 @@
-package com.tollwood.rechnungen
-
-import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
-
-
-@RepositoryRestResource(collectionResourceRel = "employee", path = "employee")
-interface EmployeeResource: PagingAndSortingRepository<Employee, Long>
