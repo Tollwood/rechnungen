241bb436148bc2d3cccc4f0bcdb1d2295ce006f3
Tobias Schlüter <tobias.schlueter@sabio.de>
2019-08-26 07:03:01 +0200

    - use typescript

---
+++ src/main/webapp/src/App.tsx
@@ -0,0 +1,45 @@
+import './App.css';
+import {Component} from "react";
+import * as React from "react";
+
+interface Article {
+  title: string
+}
+
+interface AppProps {
+  articles: Article[]
+}
+
+
+class App extends Component<{},AppProps> {
+
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { articles: [] };
+  }
+
+
+  componentDidMount() {
+
+    fetch('/article')
+        .then(response => response.json())
+        .then(data => this.setState({ articles: data._embedded.article }));
+  }
+
+
+  render() {
+
+
+    return (
+        <div className="App">
+          <ul>
+            {this.state.articles.map(article => <li>{article.title}</li>)}
+          </ul>
+
+        </div>
+    );
+
+  }
+}
+
+export default App;
