40490a4b8ae56ff982c4e2943a29fa2a569bf7f6
Tobias Schlüter <tobias.schlueter@sabio.de>
2019-11-23 13:50:22 +0100

    fix migration script

--- src/main/resources/db/migration/V1.10__drop_bill_item_id.sql
+++ src/main/resources/db/migration/V1.10__drop_bill_item_id.sql
@@ -1,11 +1,9 @@
 DELETE FROM bill_item where order_id is null;
 DELETE FROM bill_item where code is null;
 
-ALTER TABLE bill_item
-  DROP PRIMARY KEY;
+ALTER TABLE bill_item DROP CONSTRAINT bill_item_pkey;
 
-ALTER TABLE bill_item
-  DROP COLUMN id;
+ALTER TABLE bill_item DROP COLUMN id;
 
 ALTER TABLE bill_item ALTER COLUMN  code SET NOT NULL;
 ALTER TABLE bill_item ALTER COLUMN  order_id SET NOT NULL;
