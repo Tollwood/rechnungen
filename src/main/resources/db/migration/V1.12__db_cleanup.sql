caf8359894f62e1eb16690172e25180a0d7ea39c
Tobias Schlüter <tobias.schlueter@sabio.de>
2019-11-26 15:12:40 +0100

    - improve database constraints

--- src/main/resources/db/migration/V1.12__db_cleanup.sql
+++ src/main/resources/db/migration/V1.12__db_cleanup.sql
@@ -1,8 +1,16 @@
+ALTER TABLE order_table DROP CONSTRAINT IF EXISTS UK_bill_no;
+alter table order_table
+  add constraint  UK_bill_no unique (bill_no);
+
+ALTER TABLE employee DROP CONSTRAINT IF EXISTS UK_technician_id;
 ALTER TABLE employee
   add constraint UK_technician_id unique (technician_id);
 
+ALTER TABLE real_estate DROP CONSTRAINT IF EXISTS UK_name;
 ALTER TABLE real_estate
   add constraint UK_name unique (name);
+
+ALTER TABLE service DROP CONSTRAINT IF EXISTS UK_article_number;
 ALTER TABLE service
   add constraint UK_article_number unique (article_number);
 
@@ -33,7 +41,7 @@ alter table service_order
     foreign key (service_id)
       references service;
 
-
+ALTER TABLE bill_item DROP CONSTRAINT IF EXISTS FK_service_order_order_table;
 alter table bill_item
   add constraint FK_service_order_order_table
     foreign key (order_id)
--- src/main/resources/db/migration/V1.12__order_unique_billno.sql
+++
@@ -1,2 +0,0 @@
-alter table order_table
-  add constraint UK_bill_no unique (bill_no);
