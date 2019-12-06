80487a1569efd0f8675d1850d69454f7a2c0c00d
Tobias Schlüter <tobias.schlueter@sabio.de>
2019-12-06 21:20:50 +0100

    - support name

--- src/main/kotlin/com/tollwood/search/SearchController.kt
+++ src/main/kotlin/com/tollwood/search/SearchController.kt
@@ -2,6 +2,8 @@ package com.tollwood.search
 
 import com.tollwood.OrderResource
 import com.tollwood.jpa.Order
+import org.apache.lucene.search.BooleanClause.Occur
+import org.apache.lucene.search.BooleanQuery
 import org.apache.lucene.search.Query
 import org.hibernate.search.jpa.Search
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
-import org.apache.lucene.search.BooleanClause.Occur
-import org.apache.lucene.search.BooleanQuery
 
 @RestController
 class SearchController {
@@ -48,19 +48,20 @@ class SearchController {
 
         val queries = ArrayList<Query>()
 
-        for(t in terms){
+        for (t in terms) {
             queries.add(queryBuilder
                     .keyword()
                     .fuzzy()
                     .withEditDistanceUpTo(2)
                     .withPrefixLength(0)
-                    .onFields("orderId", "billNo", "realEstate.address.city", "realEstate.address.street", "realEstate.address.zipCode")
+                    .onFields("orderId", "name", "billNo", "realEstate.address.city", "realEstate.address.street", "realEstate.address" +
+                            ".zipCode")
                     .matching(t)
                     .createQuery())
         }
 
         val finalQuery = BooleanQuery.Builder()
-        for(query in queries){
+        for (query in queries) {
             finalQuery.add(query, Occur.MUST)
         }
 
