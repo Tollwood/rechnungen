3389f1f0c02d83ed7a34449cf78d5863828accd1
Tobias Schlüter <tobias.schlueter@sabio.de>
2020-03-06 22:53:28 +0100

    - no number formatting in the frontend

--- src/main/webapp/src/order/OrderList.tsx
+++ src/main/webapp/src/order/OrderList.tsx
@@ -121,10 +121,7 @@ export default class OrderList extends React.Component<OrderListProps, State> {
                 </div>
             </Table.Cell>
             <Table.Cell>{order.name ? order.name : "-"}</Table.Cell>
-            <Table.Cell>{order.sum.toLocaleString('de', {
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
-            })}</Table.Cell>
+            <Table.Cell>{order.sum}</Table.Cell>
             <Table.Cell>{(order.sum * 1.19).toLocaleString('de', {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2
